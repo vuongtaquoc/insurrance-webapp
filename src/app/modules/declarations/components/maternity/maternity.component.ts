cbf22583fc6aa4842fe669d0dbb8b6dbf1011af5
Vuong Ta Quoc <tqv.itvn@gmail.com>
2020-04-20 23:51:04 +0700

    fix tab active

--- src/app/modules/declarations/components/maternity/maternity.component.ts
+++ src/app/modules/declarations/components/maternity/maternity.component.ts
@@ -14,7 +14,7 @@ import { TABLE_NESTED_HEADERS_PART_1, TABLE_HEADER_COLUMNS_PART_1, TABLE_HEADER_
 export class MaternityComponent {
   panel: any = {
     part1: { active: true },
-    part2: { active: true }
+    part2: { active: false }
   };
   maternityHeaders: any = {
     part1: {
@@ -40,18 +40,20 @@ export class MaternityComponent {
   constructor(private declarationService: DeclarationService) {}
 
   ngOnInit() {
-    this.declarationService.getDeclarationInitials('630a', this.maternityHeaders.part1.columns).subscribe(sicknesses => {
-      this.maternity.part1.table = sicknesses;
-      this.maternity.part2.table = sicknesses;
+    this.declarationService.getDeclarationInitials('630b', this.maternityHeaders.part1.columns).subscribe(maternity => {
+      this.maternity.part1.table = maternity;
+      this.maternity.part2.table = maternity;
     });
   }
 
   collapseChange(isActive, part) {
     if (part === 'part1') {
       this.panel.part1.active = isActive;
+      this.panel.part2.active = false;
     }
 
     if (part === 'part2') {
+      this.panel.part1.active = false;
       this.panel.part2.active = isActive;
     }
   }
